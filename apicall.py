45b280de72527b158e55b04d32b371b799d27c1d
Nahleen <nahleen@mappedin.ca>
2019-02-05 12:25:45 -0500

    removed print

--- apicall.py
+++ apicall.py
@@ -2,6 +2,4 @@ from uwaterlooapi import UWaterlooAPI
 
 uw = UWaterlooAPI(api_key="c6fe03239babd8c06f887524fa0aeb12")
 
-buildings = uw.building_list()
-
-print(buildings[0])
+#buildings = uw.building_list()
