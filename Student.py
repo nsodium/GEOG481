d438cfc99b6c17d1b89e01b4cfdd7c620f67b391
Jyang2113 <46725015+Jyang2113@users.noreply.github.com>
2019-03-05 12:40:12 -0500

    Add files via upload

--- Student.py
+++ Student.py
@@ -1,26 +1,28 @@
 class Student:
-    def __init__ (self, faculty, schedule, residence, status, infectionRate, infectRoom):
-        self.faculty = faculty
-        #Schedule used to determine what building Student is in
-        self.schedule = schedule
-        self.residence = residence
-        #Status is infected status
-        self.status = status
-        #infectionRate is % chance of infecting current room
-        self.infectionRate = infectionRate
-        #infectRoom is a flag set if current room has been infected
-        self.infectRoom = infectRoom
-
-    #Determine population of room, length of class in hours,
-    #Attempt to infect room for each hour of class (random chance based
-    #on infectionRate) or until infectRoom flag is tripped.
-    #If an infection occurs, infect a random amonut of students in the room
-    def infect (room):
-        #Random check for infection
-        if (random.random() <= self.infectionRate):
-            #Infect a random number of students from 0 to the room's pop'n
-            #TODO: Replace room.population with real field name
-            for i in range(0, random.uniform(0, room.population)):
-                #TODO: Create new infected student object, will share a faculty
-                #with the current student but random schedule and residence
-
+	def __init__ (self, faculty, schedule, residence, infected, infectionRate, infectRoom):
+		self.faculty = faculty
+		#Schedule used to determine what building Student is in, TBD based on information
+		#from Waterloo API
+		self.schedule = schedule
+		self.residence = residence
+		#Infected status (true = infected, false = clean)
+		self.infected = infected
+		#infectionRate is % chance of infecting current room
+		self.infectionRate = infectionRate
+		#infectRoom is a flag set if current room has been infected
+		self.infectRoom = infectRoom
+	
+	def infect (room):
+		#Determine population of room, length of class, 
+		#Attempt to infect room each hour of class (random chance based on infectionRate)
+		#or until infectRoom flag is tripped
+		#If an infection occurs, infect a random amount of students in the room
+		if (random.random() <= self.infectionRate):
+			for i in range(0, random.uniform(1, PLACEHOLDER.roomPopulation)):
+				#Create new infected student object that shares a faculty
+				#with the current student, but random schedule and residence
+				#REPLACE PLACEHOLDER VARIABLES WITH REAL VARIABLE NAMES LATER
+				Student(self.faculty, PLACEHOLDER.schedule, PLACEHOLDER.residence,
+				true, self.infectionRate, false)
+			self.infectRoom = true
+			
